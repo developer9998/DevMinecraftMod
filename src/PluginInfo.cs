c2977f07eb4e01f5378769dd406415335367b135
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-11-05 19:54:46 -0230

    bumped version

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -7,6 +7,6 @@
 	{
 		public const string GUID = "org.dev9998.gorillatag.devminecraftmod";
 		public const string Name = "DevMinecraftMod";
-		public const string Version = "1.0.4";
+		public const string Version = "1.0.6";
 	}
 }
