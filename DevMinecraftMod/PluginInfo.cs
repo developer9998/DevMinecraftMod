d5517f2221bcee6472a7cb35b8b541926c189a2d
dev9998 <81720436+developer9998@users.noreply.github.com>
2023-07-07 09:30:21 -0230

    Update PluginInfo.cs

--- DevMinecraftMod/PluginInfo.cs
+++ DevMinecraftMod/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "org.dev9998.gorillatag.devminecraftmod";
         public const string Name = "DevMinecraftMod";
-        public const string Version = "1.1.1";
+        public const string Version = "1.1.2";
     }
 }
